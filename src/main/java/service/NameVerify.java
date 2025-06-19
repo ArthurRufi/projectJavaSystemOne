e35f0e92a2f79087964213e84c503e70be7e5200
ArthurRufi <temparthur87@gmail.com>
2025-06-19 20:49:39 -0300

    esse commit entrega uma parte da aplicação e testa o terminal git do intelijj

--- src/main/java/service/NameVerify.java
+++ src/main/java/service/NameVerify.java
@@ -1,4 +1,36 @@
 package service;
 
 public class NameVerify {
+    public String name;
+    private String[] blockedName;
+
+    public NameVerify(String name, String[] blockedName) {
+        this.name = name;
+        this.blockedName = blockedName;
+    }
+
+    public boolean nameIsBlocked(){
+        for (String blocked : blockedName){
+            if (name.equalsIgnoreCase(blocked)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getBlockedName() {
+        return blockedName;
+    }
+
+    public void setBlockedName(String[] blockedName) {
+        this.blockedName = blockedName;
+    }
 }
